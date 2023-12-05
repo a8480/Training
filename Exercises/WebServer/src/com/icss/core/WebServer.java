a79da56d2a08e27097b92d347fdde712f84b182f
onsimple <a848067526@163.com>
2023-12-05 12:56:44 +0800

    熟悉代码

--- Exercises/WebServer/src/com/icss/core/WebServer.java
+++ Exercises/WebServer/src/com/icss/core/WebServer.java
@@ -58,6 +58,8 @@ public class WebServer {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
+            } catch (RuntimeException e) {
+                throw new RuntimeException(e);
             }
         }
     }
